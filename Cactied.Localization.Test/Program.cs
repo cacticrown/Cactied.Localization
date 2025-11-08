8386a686cb73632bf54b4b82bc711108675c7cb0
cacticrown <nathan.kichler@outlook.com>
2025-11-09 00:34:14 +0100

    add example of loading a localization in code

--- Cactied.Localization.Test/Program.cs
+++ Cactied.Localization.Test/Program.cs
@@ -7,7 +7,17 @@ internal class Program
         LocalizationManager.LoadLocalizationFromFile("Translations/en.clf");
         LocalizationManager.LoadLocalizationFromFile("Translations/de.clf");
 
-        Console.WriteLine(LocalizationManager.GetLocalizedString("dialog.welcome", "de")); // this will print "Welcome to the game!", because german doesn't implement dialog.welcome and has a fallback to english
+        // Localizations can also be loading in code
+        LocalizationManager.Localizations.Add("en-uk", new Localization("en-uk", "en", new Dictionary<string, string>
+        {
+            {"menu.play", "Wanna play mate?" },
+            {"menu.quit", "Why you wanna quit mate?" },
+            {"dialog.welcome", "Welcome to great britain mate!" },
+        }));
+
+        Console.WriteLine("en: " + LocalizationManager.GetLocalizedString("dialog.welcome", "en"));
+        Console.WriteLine("en-uk: " + LocalizationManager.GetLocalizedString("dialog.welcome", "en-uk")); 
+        Console.WriteLine("de: " + LocalizationManager.GetLocalizedString("dialog.welcome", "de")); // this will print "Welcome to the game!", because german doesn't implement dialog.welcome and has a fallback to english
         Console.ReadKey();
     }
 }
